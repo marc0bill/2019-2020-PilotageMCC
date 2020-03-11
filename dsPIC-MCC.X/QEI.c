0291487a70ac2f69837626d549f7af3c0e268932
jerembipbip <jerembipbip@users.noreply.github.com>
2020-03-11 17:22:05 +0100

    Update QEI.c

--- dsPIC-MCC.X/QEI.c
+++ dsPIC-MCC.X/QEI.c
@@ -3,7 +3,14 @@
 
 void init_QEI()			// configure Encodeurs
 {
-	// to do
+	//Interruptions sur les QEI
+    IFS3bits.QEI1IF=0; //Flag 1
+    IFS4bits.QEI2IF=0; //Flag 2
+    IEC3bits.QEI1IE=1; //accepte interr QEI1
+    IEC4bits.QEI2IE=1; //accepte interr QEI2
+    RPINR15bits.INDX1R=0b01010; //Lier QEI1 au RP10 
+    RPINR17bits.INDX2R=0b10110; //Lier QEI2 au RP22
+    
 }
 
 /***************************************************************************************
